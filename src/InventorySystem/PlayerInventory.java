0c14f4238f532615dbc50e026a7bffdfbe925fc3
Lauren <sloanelaurenm@gmail.com>
2022-09-27 18:21:18 -0400

    day and night

---
+++ src/InventorySystem/PlayerInventory.java
@@ -0,0 +1,20 @@
+
+package InventorySystem;
+
+
+import java.util.ArrayList;
+
+
+public class PlayerInventory{
+	
+	public PlayerInventory() {
+		
+	}
+	
+	public ArrayList<Integer> loadPlayerInventory(){
+		ArrayList<Integer> playerInventory = new ArrayList<>(55);
+		playerInventory.add(1);
+		return playerInventory;
+	}
+	
+}
