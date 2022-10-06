87a3ce406066091ffc0d7ff11222cf293529df62
Jacob Hogrefe <jhogrefe03@gmail.com>
2022-10-06 15:32:14 -0400

    simplified collectible script class now utilizes new sound class for sounds

--- src/Scripts/TestMap/CollectibleScript.java
+++ src/Scripts/TestMap/CollectibleScript.java
@@ -1,17 +1,12 @@
 package Scripts.TestMap;
 
-import Level.ScriptState;
 import Scripts.SimpleTextScript;
-import java.io.*;
-import javax.sound.sampled.*;
-import Engine.Config;
+import Utils.Sound;
 
-public class CollectibleScript extends SimpleTextScript implements LineListener {
+public class CollectibleScript extends SimpleTextScript {
 
-    protected File collectible = new File(Config.SOUNDS_PATH + "collectible.wav");
-    protected File keyCollectible = new File(Config.SOUNDS_PATH + "keyCollectible.wav");
-    protected Clip collectibleClip;
-    protected Clip keyCollectibleClip;
+    protected Sound keyCollectible = new Sound("keyCollectible.wav", false);
+    protected Sound collectible = new Sound("keyCollectible.wav", false);
     protected String textItem;
     protected boolean playCompleted;
     protected boolean isKeyCollectible;
@@ -22,15 +17,6 @@ public class CollectibleScript extends SimpleTextScript implements LineListener 
         this.isKeyCollectible = isKeyCollectible;
     }
 
-    //determines if the playback of the sound has completed
-    @Override
-    public void update(LineEvent event) {
-        LineEvent.Type type = event.getType();
-        if (type == LineEvent.Type.STOP) {
-            playCompleted = true;
-        }
-    }
-
     @Override
     protected void setup() {
         lockPlayer();
@@ -38,29 +24,9 @@ public class CollectibleScript extends SimpleTextScript implements LineListener 
         addTextToTextboxQueue(textItem);
         //loads and plays correct audio clip depending on if it is a key collectible or not
         if (isKeyCollectible) {
-            try {
-                AudioInputStream keyStream = AudioSystem.getAudioInputStream(keyCollectible);
-                AudioFormat keyFormat = keyStream.getFormat();
-                DataLine.Info keyInfo = new DataLine.Info(Clip.class, keyFormat);
-                keyCollectibleClip = (Clip) AudioSystem.getLine(keyInfo);
-                keyCollectibleClip.addLineListener(this);
-                keyCollectibleClip.open(keyStream);
-                keyCollectibleClip.start();
-            } catch (Exception e) {
-                System.out.println("Sound file not found!!");
-            }
+            keyCollectible.play();
         } else {
-            try {
-                AudioInputStream collectibleStream = AudioSystem.getAudioInputStream(collectible);
-                AudioFormat collectibleFormat = collectibleStream.getFormat();
-                DataLine.Info collectibleInfo = new DataLine.Info(Clip.class, collectibleFormat);
-                collectibleClip = (Clip) AudioSystem.getLine(collectibleInfo);
-                collectibleClip.addLineListener(this);
-                collectibleClip.open(collectibleStream);
-                collectibleClip.start();
-            } catch (Exception e) {
-                System.out.println("Sound file not found!!");
-            }
+            collectible.play();
         }
     }
 }
