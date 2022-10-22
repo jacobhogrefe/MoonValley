ccc07bb22175010ecff83ee7263c2b1a57dfb0d9
Jacob Hogrefe <jhogrefe03@gmail.com>
2022-10-21 22:47:28 -0400

    testMap now biomeStart

--- src/Scripts/TestMap/ExitHouseScript.java
+++ src/Scripts/TestMap/ExitHouseScript.java
@@ -4,6 +4,7 @@ import Game.Game;
 import Level.Script;
 import Level.ScriptState;
 import Maps.TestMap;
+import Maps.Biomes.BiomeStart;
 
 // trigger script at beginning of game to set that heavy emotional plot
 public class ExitHouseScript extends Script {
@@ -21,7 +22,7 @@ public class ExitHouseScript extends Script {
 
 	@Override
 	public ScriptState execute() {
-		Game.getRunningInstance().getScreenCoordinator().getPlayLevelScreen().teleport(new TestMap(), 550, 1000);
+		Game.getRunningInstance().getScreenCoordinator().getPlayLevelScreen().teleport(new BiomeStart(), 550, 1000);
 		return ScriptState.COMPLETED;
 	}
 }
