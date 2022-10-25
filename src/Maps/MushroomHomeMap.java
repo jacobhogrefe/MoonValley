6b0f8ae6d094476c952c4e45c601f53e44b9b720
ellidelli <elizabeth.delea@gmail.com>
2022-10-25 12:20:06 -0400

    added mushroom man npc

--- src/Maps/MushroomHomeMap.java
+++ src/Maps/MushroomHomeMap.java
@@ -1,7 +1,10 @@
 package Maps;
 
+import java.util.ArrayList;
 import Level.Map;
 import Level.MusicState;
+import Level.NPC;
+import NPCs.MushroomMan;
 import Tilesets.MushroomHomeTileset;
 
 // Represents a test map to be used in a level
@@ -10,6 +13,17 @@ public class MushroomHomeMap extends Map {
     public MushroomHomeMap() {
         super("mushroom_home_map.txt", new MushroomHomeTileset());
         this.playerStartPosition = getMapTile(5, 5).getLocation();
+    }
+    
+    @Override
+    public ArrayList<NPC> loadNPCs() {
+        ArrayList<NPC> npcs = new ArrayList<>();
+
+        MushroomMan mushroomMan = new MushroomMan(1, getMapTile(8, 7).getLocation().subtractY(40));
+//        mushroomMan.setInteractScript(new WalrusScript());
+        npcs.add(mushroomMan);
+
+        return npcs;
     }
     
     @Override
