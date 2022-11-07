9e140d3419885c92f8af0bc9c82a4f55721c7606
Matt Z <matthew.zingarella@gmail.com>
2022-11-07 16:53:44 -0500

    Pushing error when enetering desert for troubleshooting by team

--- src/Maps/Biomes/BiomeDesert.java
+++ src/Maps/Biomes/BiomeDesert.java
@@ -1,12 +1,19 @@
 package Maps.Biomes;
 
+import java.util.ArrayList;
 import java.util.function.Supplier;
 
 import Level.Map;
 import Level.MusicState;
+import Level.NPC;
 import Maps.AbstractLoopingMap;
+import NPCs.Cattle;
+import NPCs.Dinosaur;
+import NPCs.Walrus;
 import Registry.ItemRegistry;
 import Registry.ItemRegistry.Item;
+import Scripts.TestMap.DinoScript;
+import Scripts.TestMap.WalrusScript;
 import Tilesets.CommonTileset;
 import Tilesets.DesertTileset;
 import Utils.Side;
@@ -45,6 +52,17 @@ public class BiomeDesert extends AbstractLoopingMap {
             default:
                 return null;
         }
+    }
+    
+    @Override
+    public ArrayList<NPC> loadNPCs() {
+        ArrayList<NPC> npcs = new ArrayList<>();
+
+        Cattle cattle1 = new Cattle(3, getMapTile(10, 10).getLocation());
+        npcs.add(cattle1);
+
+
+        return npcs;
     }
 
     @Override
---
+++ src/NPCs/Cattle.java
@@ -0,0 +1,78 @@
+package NPCs;
+
+import Builders.FrameBuilder;
+import Engine.GraphicsHandler;
+import Engine.ImageLoader;
+import GameObject.Frame;
+import GameObject.ImageEffect;
+import GameObject.SpriteSheet;
+import Level.NPC;
+import Utils.Point;
+
+import java.util.HashMap;
+
+// This class is for the dinosaur NPC
+public class Cattle extends NPC {
+
+    public Cattle(int id, Point location) {
+        super(id, location.x, location.y, new SpriteSheet(ImageLoader.load("Cattle2.png"), 15, 15), "STAND_RIGHT");
+    }
+
+    @Override
+    public HashMap<String, Frame[]> loadAnimations(SpriteSheet spriteSheet) {
+        return new HashMap<String, Frame[]>() {{
+            put("STAND_RIGHT", new Frame[] {
+                    new FrameBuilder(spriteSheet.getSprite(0, 0))
+                            .withScale(1)
+                            .withBounds(4, 5, 5, 10)
+                            .build()
+            });
+            put("STAND_LEFT", new Frame[] {
+                   new FrameBuilder(spriteSheet.getSprite(0, 0))
+                           .withScale(1)
+                           .withBounds(4, 5, 5, 10)
+                           .withImageEffect(ImageEffect.FLIP_HORIZONTAL)
+                           .build()
+           });
+
+            put("WALK_RIGHT", new Frame[]{
+                    new FrameBuilder(spriteSheet.getSprite(0, 1), 200)
+                            .withScale(1)
+                            .withBounds(4, 5, 5, 10)
+                            .build(),
+                    new FrameBuilder(spriteSheet.getSprite(1, 1), 200)
+                            .withScale(1)
+                            .withBounds(4, 5, 5, 10)
+                            .build(),
+                   new FrameBuilder(spriteSheet.getSprite(2, 1), 200)
+                            .withScale(1)
+                            .withBounds(4, 5, 5, 10)
+                            .build()
+                            
+            });
+
+            put("WALK_LEFT", new Frame[]{
+                    new FrameBuilder(spriteSheet.getSprite(0, 1), 200)
+                    .withScale(1)
+                    .withImageEffect(ImageEffect.FLIP_HORIZONTAL)
+                    .withBounds(4, 5, 5, 10)
+                    .build(),
+                    new FrameBuilder(spriteSheet.getSprite(1, 1), 200)
+                    .withScale(1)
+                    .withImageEffect(ImageEffect.FLIP_HORIZONTAL)
+                    .withBounds(4, 5, 5, 10)
+                    .build(),
+                    new FrameBuilder(spriteSheet.getSprite(2, 1), 200)
+                    .withScale(1)
+                    .withImageEffect(ImageEffect.FLIP_HORIZONTAL)
+                    .withBounds(4, 5, 5, 10)
+                    .build()
+            });
+        }};
+    }
+
+    @Override
+    public void draw(GraphicsHandler graphicsHandler) {
+        super.draw(graphicsHandler);
+    }
+}
