0c1571df7c02c404aa39c2d875f9b98ec8a3bb91
Jacob Hogrefe <jhogrefe03@gmail.com>
2022-11-18 01:08:53 -0500

    better comments

--- src/Scripts/SmartMapTeleportScript.java
+++ src/Scripts/SmartMapTeleportScript.java
@@ -87,22 +87,26 @@ public class SmartMapTeleportScript extends MapTeleportScript {
         }
     }
 
-    //checks if the required item is null, if it is it'll teleport to the right map (start map)
-    //if not, it'll check if the player has the item and if they don't it'll show the textbox and if they do it'll teleport them to the right area
     @Override
     protected ScriptState execute() {
+        //checks if the required item is null or not
         if (this.requiredItem != null) {
+            //checks if the player has the right item
             if (!Game.getRunningInstance().getScreenCoordinator().getPlayLevelScreen().getPlayerInventory().containsItem(this.requiredItem)) {
+                //shows the textbox indicating the correct item needed
                 start();
                 if (!isTextboxQueueEmpty()) {
                     return ScriptState.RUNNING;
                 }
+            //sends the player to the correct map
             } else {
                 return super.execute();
             }
+        //start map is the only map with no required item, thus this case exists
         } else if (this.requiredItem == null) {
             return super.execute();
         }
+        //ends the script
         end();
         return ScriptState.COMPLETED;
     }
