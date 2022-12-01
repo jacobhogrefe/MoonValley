4211d914a7c6cf0423fc441da5fe24e95307ecb7
Jacob Hogrefe <jhogrefe03@gmail.com>
2022-12-01 14:59:59 -0500

    modifed credits screen to have our names

--- src/Screens/CreditsScreen.java
+++ src/Screens/CreditsScreen.java
@@ -12,9 +12,7 @@ import java.awt.*;
 public class CreditsScreen extends Screen {
     protected ScreenCoordinator screenCoordinator;
     protected Map background;
-    protected SpriteFont creditsLabel;
-    protected SpriteFont createdByLabel;
-    protected SpriteFont returnInstructionsLabel;
+    protected SpriteFont creditsLabel, createdByLabel, improvedByLabel, returnInstructionsLabel, hogrefeLabel, mattLabel, ellieLabel, laurenLabel;
 
     public CreditsScreen(ScreenCoordinator screenCoordinator) {
         this.screenCoordinator = screenCoordinator;
@@ -27,6 +25,11 @@ public class CreditsScreen extends Screen {
         background.setAdjustCamera(false);
         creditsLabel = new SpriteFont("Credits", 15, 35, "Times New Roman", 30, Color.white);
         createdByLabel = new SpriteFont("Created by Alex Thimineur", 130, 140, "Times New Roman", 20, Color.white);
+        improvedByLabel = new SpriteFont("Improved by:", 130, 170, "Times New Roman", 20, Color.white);
+        hogrefeLabel = new SpriteFont("Jacob Hogrefe", 170, 200, "Times New Roman", 20, Color.white);
+        mattLabel = new SpriteFont("Matthew Zingarella", 170, 230, "Times New Roman", 20, Color.white);
+        ellieLabel = new SpriteFont("Ellie Delea", 170, 260, "Times New Roman", 20, Color.white);
+        laurenLabel = new SpriteFont("Lauren Sloane", 170, 290, "Times New Roman", 20, Color.white);
         returnInstructionsLabel = new SpriteFont("Press Space to return to the menu", 20, 560, "Times New Roman", 30, Color.white);
     }
 
@@ -44,7 +47,12 @@ public class CreditsScreen extends Screen {
     public void draw(GraphicsHandler graphicsHandler) {
         background.draw(graphicsHandler);
         creditsLabel.draw(graphicsHandler);
-        createdByLabel.drawWithParsedNewLines(graphicsHandler);
+        createdByLabel.draw(graphicsHandler);
+        improvedByLabel.draw(graphicsHandler);
+        hogrefeLabel.draw(graphicsHandler);
+        mattLabel.draw(graphicsHandler);
+        ellieLabel.draw(graphicsHandler);
+        laurenLabel.draw(graphicsHandler);
         returnInstructionsLabel.draw(graphicsHandler);
     }
 }
